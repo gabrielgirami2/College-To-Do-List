94807751f21a05c9ddd409ce9d05b766084fa591
Gabriel Girami <rm98017@fiap.com.br>
2024-03-17 19:52:12 -0300

    configs on taskfyApplication

--- src/main/java/com/fiap/taskfy/TaskfyApplication.java
+++ src/main/java/com/fiap/taskfy/TaskfyApplication.java
@@ -2,12 +2,22 @@ package com.fiap.taskfy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
+@Controller
 public class TaskfyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaskfyApplication.class, args);
 	}
 
+
+	@RequestMapping
+	@ResponseBody
+	public String home(){
+		return"Taskfy";
+	}
 }
